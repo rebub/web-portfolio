aba10fd65496d3d9b0cacd0375d1c241257d79db
Sandro <rebub@vivaldi.net>
2021-12-19 03:35:38 +0000

    Major additions
    Added hero section along with tweaks to navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,35 +1,10 @@
-$(() => {
-    //On Scroll Functionality
-    $(window).scroll(() => {
-        var windowTop = $(window).scrollTop();
-        windowTop > 100 ?
-            $("nav").addClass("navSize") :
-            $("nav").removeClass("navSize");
-    });
+// toggle dropdown menu
+var dropdown = document.getElementById("dropdown-menu")
 
-    //Click title To Scroll To Top
-    $("#title")
-        .add("#logo")
-        .on("click", () => {
-            $("html,body").animate({
-                    scrollTop: 0,
-                },
-                150
-            );
-        });
+document.getElementById("dropdown-hamburguer").addEventListener("click", function () {
+    dropdown.style.display = dropdown.style.display === "none" ? "" : "none";
+}, false);
 
-
-    //Toggle Menu
-    $("#menu-toggle").on("click", () => {
-        $("#menu-toggle").toggleClass("closeMenu");
-        $("ul").toggleClass("showMenu");
-        $("nav").removeClass("navShadow");
-        $("nav").css("border-radius", "0 0 10px 10px;");
-
-        $("li").on("click", () => {
-            $("#menu-toggle").removeClass("closeMenu");
-            $("nav").addClass("navShadow")
-            $("ul").removeClass("showMenu");
-        });
-    });
-});
+document.getElementById("dropdown-close").addEventListener("click", function () {
+    dropdown.style.display = dropdown.style.display === "none" ? "" : "none";
+}, false);
