52cc015018e0dbd7fbb8ccf8f62b9d662d7f6e08
Sandro <rebub@vivaldi.net>
2021-12-30 15:41:55 +0000

    Added cta section

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,25 @@
+$(() => {
+    //On Scroll Functionality
+    $(window).scroll(() => {
+        var windowTop = $(window).scrollTop();
+        windowTop > 100 ?
+            $("nav").addClass("navbar-addon") :
+            $("nav").removeClass("navbar-addon");
+    });
+
+    //Click title To Scroll To Top
+    $("#title")
+        .add("#logo")
+        .on("click", () => {
+            $("html,body").animate({
+                    scrollTop: 0,
+                },
+                150
+            );
+        });
+});
+
+
 // toggle dropdown menu
 var dropdown = document.getElementById("dropdown-menu")
 
